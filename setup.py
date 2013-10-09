d9c9194288cb0125393e0b157bd5e73b3554a031
Kamil Madac <kamil.madac@gmail.com>
2013-10-09 23:44:51 +0200

    fixed semond dameon script name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='SeMon',
-    version='0.1-9-g6dbbf94-131009233755',
+    version='0.1-10-g8b991dc-131009234347',
     packages=['SeMon', 'Web'],
     package_data={'Web': ['static/*', 'templates/*']},
     scripts=['bin/semond.py'],
