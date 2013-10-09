8b991dc1bf88b0165fdf696a332f4a158b6afe33
Kamil Madac <kamil.madac@gmail.com>
2013-10-09 23:43:32 +0200

    correct supervisor ini template added

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='SeMon',
-    version='0.1-8-g1629086-131009000947',
+    version='0.1-9-g6dbbf94-131009233755',
     packages=['SeMon', 'Web'],
     package_data={'Web': ['static/*', 'templates/*']},
     scripts=['bin/semond.py'],
